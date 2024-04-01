ce23ef6dc080a9253ddffa456cba6cb2664e4556
Lebo Madikane <madikanelebohang@gmail.com>
2024-04-01 20:53:08 +0200

    create a C program that finds the largest number

--- 1-flow-control/2-largest-number.c
+++ 1-flow-control/2-largest-number.c
@@ -4,6 +4,22 @@
 
 int main(void) {
 
-retuen 0;
+int n1, n2, n3;
+
+printf("Enter three different numbers: ");
+scanf("%d %d %d", &n1, &n2, &n3);
+
+if (n1 > n2 && n1 > n3)
+	printf("%d is the largest number\n", n1);
+
+
+if (n2 > n1 && n2 > n3)
+	printf("%d is the largest number\n", n2);
+
+
+if (n3 > n1 && n3 > n2)
+	printf("%d is the largest number\n", n3);
+
+return 0;
 
 };
