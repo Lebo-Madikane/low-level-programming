442b5e19402423678b88306041046e343bdb983e
Lebo Madikane <madikanelebohang@gmail.com>
2024-04-07 00:30:19 +0200

    create a C program to find the ASCII value of the entered character

---
+++ 2-function/2-ASCII.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// main - A C program to find ASCII value of entered character
+
+int main(void) {
+
+char c;
+
+printf("Enter a character: ");
+scanf("%c", &c);
+
+printf("The ASCII value of %c is %d\n", c, c);
+
+return 0;
+
+}
