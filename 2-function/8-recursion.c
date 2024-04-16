fd5abc7c6ec34bfc7ae59140b8b9d86c82770895
Lebo Madikane <madikanelebohang@gmail.com>
2024-04-16 11:04:53 +0200

    create a c program that calls itself

---
+++ 2-function/8-recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// main - A c program that calls a recursion function.
+// recursion - A function that calls itself.
+
+int recursion(void) {
+
+return 0;
+
+}
+
+int main(void) {
+
+return 0;
+
+}
