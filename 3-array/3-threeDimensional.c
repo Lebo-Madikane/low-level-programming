e942d6e015b7fc218d51513585950ec273166e07
Lebo Madikane <madikanelebohang@gmail.com>
2024-05-02 10:03:19 +0200

    create a c program and declare a three dimensional array

---
+++ 3-array/3-threeDimensional.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// main - A program declaring a three-dimensional array(3D).
+
+int main(void) {
+
+float y[2][4][3]; //Here the array y can hold 24 elements.
+
+return 0;
+
+}
