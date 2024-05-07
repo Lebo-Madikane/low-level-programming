ff469049c5344b9ff453c911aa635abc4b8369b5
Lebo Madikane <madikanelebohang@gmail.com>
2024-05-07 09:45:45 +0200

    create a c program syntax on how to assign an address to a pointer variable

---
+++ 4-pointers/1-assigningAddress.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// main - A c program to assigning an address to a pointer variable.
+
+int main(void) {
+
+int *p, c; //declaring a pointer *p and a variable c
+c = 5;  //assigning value to variable c
+p = &c; //address of c is assigned to pointer p
+
+return 0;
+
+}
